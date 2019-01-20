f610b90f82402d527629cefc82dd73a7c791aa1a
Christian Bartl <christian.bartl.1981@gmail.com>
2019-01-20 19:52:01 +0100

    transform to a project layout template.

--- project_name/project_name.py
+++ project_name/project_name.py
@@ -5,7 +5,7 @@ from django.conf import settings
 
 DEBUG = os.environ.get('DEBUG', 'on') == 'on'
 
-SECRET_KEY = os.environ.get('SECRET_REY', os.urandom(32))
+SECRET_KEY = os.environ.get('SECRET_REY', '{{ secret_key }}')
 
 ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhose').split(',')
 
