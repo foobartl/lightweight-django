9d0e02e6ec3c829df2a493aa9e7caad84fc0f598
Christian Bartl <cb@it-agenten.com>
2019-05-24 20:37:59 +0200

    take image data from url and validate

--- placeholder/placeholder.py
+++ placeholder/placeholder.py
@@ -23,16 +23,35 @@ settings.configure(
 )
 
 
+from django import forms
 from django.conf.urls import url
 from django.core.wsgi import get_wsgi_application
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseBadRequest
+
+
+class ImageForm(forms.Form):
+    """Form to validate requested placeholder image."""
+
+    height = forms.IntegerField(min_value=1, max_value=2000)
+    width = forms.IntegerField(min_value=1, max_value=2000)
+
+def placeholder(request, width, height):
+    form = ImageForm({'height': height, 'width': width})
+    if form.is_valid():
+        height = form.cleaned_data['height']
+        width = form.cleaned_data['width']
+        # TODO: Generate image of requested size
+        return HttpResponse('Ok')
+    else:
+        return HttpResponseBadRequest('Invalid Image Request')
 
 
 def index(request):
     return HttpResponse('Hello World')
 
 urlpatterns = (
-    url(r'^$', index),        
+    url(r'^image/(?P<width>[0-9]+)x(?P<height>[0-9]+)/$', placeholder, name='placeholder'),
+    url(r'^$', index, name='homepage'),
 )
 
 application = get_wsgi_application()
