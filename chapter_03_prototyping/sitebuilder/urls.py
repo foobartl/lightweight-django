cce6bf1e77e4c10c819b560aae24455802d511e3
Christian Bartl <cb@it-agenten.com>
2019-05-25 20:22:34 +0200

    add the urls file

--- chapter_03_prototyping/sitebuilder/urls.py
+++ chapter_03_prototyping/sitebuilder/urls.py
@@ -1,6 +1,11 @@
+from django.conf.urls import url
 
+from .views import page
 
-urlpatterns = ()
+urlpatterns = (
+    url(r'^(?P<slug>[\w./-]+)/$', page, name='page'),
+    url(r'^$', page, name='homepage'),
+)
 
 
 
