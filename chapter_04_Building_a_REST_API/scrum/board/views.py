3273419c54fd2f9ff0c890cf3d35033e9cbff589
Christian Bartl <cb@it-agenten.com>
2019-06-06 16:39:13 +0200

    add ViewSet

--- chapter_04_Building_a_REST_API/scrum/board/views.py
+++ chapter_04_Building_a_REST_API/scrum/board/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from .serializers import SprintSerializer
+from .models import Sprint
+
+
+class SprintViewSet(viewsets.ModelViewSet):
+    """API endpoint for listing and creating sprints."""
+
+    queryset = Sprint.objects.order_by('end')
+    serializer_class = SprintSerializer
