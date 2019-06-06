4055fa48b50fcee088fe0474525f71ebf663b0a1
Christian Bartl <cb@it-agenten.com>
2019-06-06 18:57:21 +0200

    connect the app to the router

--- chapter_04_Building_a_REST_API/scrum/scrum/urls.py
+++ chapter_04_Building_a_REST_API/scrum/scrum/urls.py
@@ -14,9 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
-from django.urls import path
+from django.urls import path, include
 from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
     path('api/token/', obtain_auth_token, name='api-token'),
+    path('api', include('router.urls')),
+
 ]
