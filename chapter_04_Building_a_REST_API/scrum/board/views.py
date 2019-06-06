750d1dd7d11680a97dd79c6980ec02e192825dfb
Christian Bartl <cb@it-agenten.com>
2019-06-06 18:42:30 +0200

    add serializer and view set for User

--- chapter_04_Building_a_REST_API/scrum/board/views.py
+++ chapter_04_Building_a_REST_API/scrum/board/views.py
@@ -1,8 +1,10 @@
+from django.contrib.auth import get_user_model
 from rest_framework import viewsets, authentication, permissions
 
-from .serializers import SprintSerializer
-from .models import Sprint
+from .serializers import SprintSerializer, TaskSerializer, UserSerializer
+from .models import Sprint, Task
 
+User = get_user_model()
 
 class DefaultsMixin(object):
     """Default settings for view authentication, permissions, filtering and pagination."""
@@ -24,3 +26,20 @@ class SprintViewSet(DefaultsMixin, viewsets.ModelViewSet):
 
     queryset = Sprint.objects.order_by('end')
     serializer_class = SprintSerializer
+
+
+class TaskViewSet(DefaultsMixin, viewsets.ModelViewSet):
+    """API endpoint for listing and creating tasks."""
+
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
+
+
+class UserViewSet(DefaultsMixin, viewsets.ReadOnlyModelViewSet):
+    """API endpoint for listing users."""
+
+    lookup_field = User.USERNAME_FIELD
+    lookup_url_kwarg = User.USERNAME_FIELD
+    queryset = User.objects.order_by(User.USERNAME_FIELD)
+    serializer_class = UserSerializer
+
